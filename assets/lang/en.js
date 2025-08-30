bfba7e50869d78a065a9d646b409c6ad310ebb4a
ptrbrs <4799026@gmail.com>
2025-08-31 00:17:48 +0200

    Delete assets directory

--- assets/lang/en.js
+++
@@ -1,7 +0,0 @@
-export default {
-    nav_home: "home",
-    nav_works: "works",
-    nav_about: "about",
-    home_title: "<span>say</span> <span>YES</span> <span>2</span> <span>no!</span>",
-    home_desc: "question. challenge. idea. implementation. answer.",
-};
--- assets/lang/es.js
+++
@@ -1,7 +0,0 @@
-export default {
-    nav_home: "inicio",
-    nav_works: "trabajos",
-    nav_about: "sobre mi",
-    home_title: "<span>¡di</span> <span>SÍ</span> <span>a</span> <span>no!</span>",
-    home_desc: "pregunta. desafío. idea. implementación. respuesta.",
-};
